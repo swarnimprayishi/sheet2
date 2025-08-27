cf1054a9e2c0e685ab1a8e4b484580ed177dbeb5
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-27 12:31:18 +0530

    Update p9_sumdigit.py

--- p9_sumdigit.py
+++ p9_sumdigit.py
@@ -3,9 +3,10 @@ sum_digits = 0
 
 while N > 0:
     digit = N % 10      
-    sum_digits += digit
+    sum_digits = sum_digits + digit
     N //= 10            
 
 print("Sum of digits:", sum_digits)
+
 
 # take an integer N as input.your task is to calculate and print the sum of the digits of the given number N
