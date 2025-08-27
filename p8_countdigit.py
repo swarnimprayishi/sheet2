7ce6509fcdb52a87820210e5b3cdca3cc63933fc
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-27 12:31:58 +0530

    Update p8_countdigit.py

--- p8_countdigit.py
+++ p8_countdigit.py
@@ -6,10 +6,11 @@ if num == 0:
     count = 1
 else:
     while num > 0:
-        count += 1
+        count = count + 1
         num //= 10
 
 print("Count of digits:", count)
 
 
 # take an integer N as input and print the count of that number
+
