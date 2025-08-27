53879f031db2174642e5c4077619333cae989c18
swarnimprayishi <swarnimprayishi@gmail.com>
2025-08-27 12:26:52 +0530

    Update p12_integers.py

--- p12_integers.py
+++ p12_integers.py
@@ -1,10 +1,8 @@
-A = int(input("Enter A: "))
-B = int(input("Enter B: "))
-result = 1
 
-for i in range(B):
-    result *= A
+A = int(input("Enter the base : "))
+B = int(input("Enter the exponent: "))
+result = A ** B
+print("A^B =", result)
 
-print("Result:", result)
 
 #you are given two integers A and B. you have to find the value of A^B.
